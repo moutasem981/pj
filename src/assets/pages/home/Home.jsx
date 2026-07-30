a51ec2d091d3f01ad96d2f53ba0bd7022febdfdd
moutasem981 <emadmoutasem0@gmail.com>
2026-07-30 23:06:49 +0300

    update font size

--- src/assets/pages/home/Home.jsx
+++ src/assets/pages/home/Home.jsx
@@ -12,14 +12,14 @@ const {t} = useTranslation();
 const navigate = useNavigate();
 
   return (
-      <>
+      <main className='md:relative  z-10 '>
      {/* hero section */}
-       <section className='bg-primary-bg md:pt-30 pt-15 pb-10 md:pb-25 md:relative  z-10  '>
+       <section className='bg-primary-bg md:pt-30 pt-15 pb-10 md:pb-25  '>
         <div className='container flex justify-between items-center flex-wrap md:flex-nowrap '>
           <div className='md:text-start text-center md:mx-0 mx-auto '>
             <div>
             <h1 className=''>{t('Discover Everything You Need')}</h1>
-            <p className='text-main text-white/70  w-7/10 md:mx-0 mx-auto  '>
+            <p className='text-main text-white/70  md:w-7/10 md:mx-0 mx-auto  '>
             {t('Your search ends here: unlock exclusive savings, experience effortless shopping, and find top-rated items for your')} :
            <Typewriter component='div' className='font-bold '
              options={{
@@ -34,14 +34,20 @@ const navigate = useNavigate();
             <button onClick={()=> navigate('/Products')} className='btn-start mt-8 md:mb-0 mb-4 md:mt-12'>{t('GET STARTED')}</button>
           </div>
           <div className='md:max-w-4/10  '>
-            <img className={`md:absolute md:top-40 lg:top-22  mx-auto w-8/10 md:w-4/10 ${i18n.dir() === "rtl" ? "left-0 " : "right-0"}`}  src={heroImg} alt="image hero section"  /> 
+            <img className={`md:absolute md:top-40 lg:top-22  mx-auto w-full md:w-4/10 max-w-175  ${i18n.dir() === "rtl" ? "left-0 " : "right-0"}`}  src={heroImg} alt="image hero section"  /> 
           </div>
         </div>
        </section>
-       <section className='h-[600px]'>
+
+
+         {/* Features section */}
+       <section className='flex justify-center sm:justify-around gap-y-3 items-center   h-[600px]'>
+        <div className='h-100 bg-primary-bg'>saxs</div>
+        <div  className='h-100 bg-primary-bg'>xsax</div>
+        <div  className='h-100 bg-primary-bg'>sxaxas</div>
        </section>
 
-      </>
+      </main>
     
 
   )
