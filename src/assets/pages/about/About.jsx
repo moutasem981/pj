ff672a7f6b845d87c34618221e8e0fb178101d6b
moutasem981 <emadmoutasem0@gmail.com>
2026-08-27 21:05:19 +0300

    creating Mission section

--- src/assets/pages/about/About.jsx
+++ src/assets/pages/about/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import heroImg from '@/img/about/hero.png'
+import missionImg from '@/img/about/mission.webp'
 
 export default function About() {
   const { t } = useTranslation();
@@ -21,8 +22,8 @@ export default function About() {
             <h1>{t('About Us')}</h1>
           </div>
 
-          <div className='relative w-full md:w-7/10 bg-white'>
-            <img src={heroImg} alt={t('About Us')} className='w-full h-full object-cover' />
+          <div className='relative max-md:mx-auto w-screen   md:w-7/10 bg-white'>
+            <img src={heroImg} alt={t('About Us')} className='w-screen h-full object-cover' />
           </div>
         </div>
 
@@ -47,6 +48,23 @@ export default function About() {
 
           </div>
         </div>
+      </section>
+
+      {/* Our Mission And Vision section */}
+      <section className=' mt-5 md:mt-20 py-10 lg:py-25 bg-primary-bg container min-h-112.5'>
+   
+        <div className='grid md:grid-cols-2 gap-10 justify-center items-center  relative'>
+          <div className='md:w-5/10'>
+             <img src={missionImg} alt={t('Our Mission And Vision')} className=' lg:absolute md:max-lg:max-w-87 max-md:mx-auto  -bottom-10/10  lg:-top-10/10' />
+          </div>
+          <div >
+            <span className='text-primary-addres text-[13px] font-semibold tracking-[2px]'> {t('CORE VALUE COMPANY')} </span>
+            <h2 className='text-white mt-3 mb-5'>{t('Our Mission And Vision')}</h2>
+            <p className='text-primary-addres text-main'>{t("There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, buying to injected humour, or randomised words which don't look even many desktop publishing packages.")} </p>
+          </div>
+
+        </div>
+        
       </section>
     </main>
   )
