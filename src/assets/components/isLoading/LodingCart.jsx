0a45049481c63c48a5f33d9aa7d3cc4381d31380
moutasem hassan <emadmoutasem0@gmail.com>
2026-08-30 22:45:01 +0300

    Update LodingCart.jsx

--- src/assets/components/isLoading/LodingCart.jsx
+++ src/assets/components/isLoading/LodingCart.jsx
@@ -1,6 +1,7 @@
 import useCart from '@/assets/hooks/useCart';
 import React from 'react'
-
+import { Card, CardContent } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
 export default function LodingCart() {
 
   const { data } = useCart();
